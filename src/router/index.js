6f9a6a62e51044dcea69d7fb5d86ab89d045a9d6
Gourav Jana <143085765+rickxzo@users.noreply.github.com>
2026-06-02 09:39:05 +0530

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHashHistory, createWebHistory } from 'vue-router'
 import Home from "../components/home.vue"
 import NotFound from "../components/notfound.vue"
 import Contact from "../components/contact.vue"
