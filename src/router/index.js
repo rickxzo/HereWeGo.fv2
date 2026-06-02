80a0764f04096b0cc4f453728ecd9035dd0222ff
Gourav Jana <143085765+rickxzo@users.noreply.github.com>
2026-06-02 09:36:12 +0530

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,7 @@ import SignOut from "../components/signout.vue"
 
 
 const router = createRouter({
-  history: createWebHashHistory(import.meta.env.BASE_URL),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
