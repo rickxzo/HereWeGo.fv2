7f9012fccb1f52588fdec62611aeeb4bebe0e84f
Gourav Jana <143085765+rickxzo@users.noreply.github.com>
2026-06-03 19:17:21 +0530

    Create auth.vue

---
+++ src/components/auth.vue
@@ -0,0 +1,28 @@
+<!-- AuthCallback.vue -->
+<script setup>
+import { onMounted } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+
+const route = useRoute()
+const router = useRouter()
+
+onMounted(() => {
+  const token = route.query.token
+  const id = route.query.id
+  const username = route.query.username
+  const avatar = route.query.avatar
+
+  if (token) {
+    localStorage.setItem('token', token)
+    localStorage.setItem('userId', id)
+    localStorage.setItem('username', username)
+    localStorage.setItem('avatar', avatar)
+  }
+
+  router.replace('/dashboard')
+})
+</script>
+
+<template>
+  <div>Signing you in...</div>
+</template>
