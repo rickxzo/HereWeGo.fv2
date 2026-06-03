60e50b79f0d31de8185ca13fc845452c77fb1511
Gourav Jana <143085765+rickxzo@users.noreply.github.com>
2026-06-03 19:26:38 +0530

    Add GitHub redirect component in github.vue

---
+++ src/components/github.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="flex items-center justify-center min-h-screen">
+    <p>Redirecting to GitHub...</p>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+
+const connectGithub = () => {
+  window.location.href = 'https://herewego-3kgp.onrender.com/login/github'
+}
+
+onMounted(() => {
+  connectGithub()
+})
+</script>
